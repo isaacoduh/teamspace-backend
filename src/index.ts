9251e536837c08398b837b2c54deaa7e7cfae409
Isaac Oduh <ufuomaoduh@gmail.com>
2025-02-06 07:07:58 +0000

    implements workspace feature

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ import passport from "passport";
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
+import workspaceRoutes from "./routes/workspace.route";
 import memberRoutes from "./routes/member.route";
 
 const app = express();
@@ -47,13 +48,14 @@ app.get(
       ErrorCodeEnum.AUTH_INVALID_TOKEN
     );
     return res.status(HTTPSTATUS.OK).json({
-      message: "Hello Subscribe to the channel & share",
+      message: "Welcome... API Endpoints Base",
     });
   })
 );
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
+app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
 app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
 
 app.use(errorHandler);
